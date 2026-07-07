0cf00472f2102b5f3eb7f9e3c51ed77549ff5f85
Gursharan Singh <gursharansinghsood@gmail.com>
2026-07-07 11:41:53 +0530

    Update terminal font family

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext.jsx";
-import { IconProvider } from "./context/IconContext.jsx";
+import { ThemeProvider } from "./context/ThemeProvider.jsx";
+import { IconProvider } from "./context/IconProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- client/src/pages/TerminalPage.jsx
+++ client/src/pages/TerminalPage.jsx
@@ -1,32 +1,30 @@
-import  { useEffect, useRef, useState } from 'react'
-import Titlebar from '../components/cards/Titlebar'
-import InputSystem from '../components/terminal/InputSystem'
-import Fade from '../components/motion/Fade'
-import HistorySystem from '../components/terminal/HistorySystem'
+import { useEffect, useRef, useState } from "react";
+import Titlebar from "../components/cards/Titlebar";
+import InputSystem from "../components/terminal/InputSystem";
+import Fade from "../components/motion/Fade";
+import HistorySystem from "../components/terminal/HistorySystem";
 
 const TerminalPage = () => {
+  const [history, setHistory] = useState([]);
 
-    const [history, setHistory] = useState([])
+  const bottomRef = useRef(null);
 
-    const bottomRef = useRef(null)
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [history]);
 
-    useEffect(() => {
-        bottomRef.current?.scrollIntoView({
-            behavior: "smooth",
-        })
-    },[history])
+  return (
+    <Fade className="h-screen flex flex-col">
+      <Titlebar name={"Terminal"} />
+      <div className="flex-1 overflow-y-auto p-5 font-terminal">
+        <HistorySystem history={history} />
+        <InputSystem history={history} setHistory={setHistory} />
+        <div ref={bottomRef} />
+      </div>
+    </Fade>
+  );
+};
 
-
-    return (
-        <Fade className='h-screen flex flex-col'>
-            <Titlebar name={"Terminal"} />
-            <div className='flex-1 overflow-y-auto p-5'>
-                <HistorySystem history={history} />
-                <InputSystem history={history} setHistory={setHistory} />
-                <div ref={bottomRef} />
-            </div>
-        </Fade>
-    )
-}
-
-export default TerminalPage
+export default TerminalPage;
